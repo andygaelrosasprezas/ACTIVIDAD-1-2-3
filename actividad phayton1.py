a147cf2be05f039e7a624e1ac0b1441aec67fecb
andygaelrosasprezas <andygaelrosaspreza@gmail.com>
2024-09-12 07:57:40 -0600

    un nuevo archivo

---
+++ actividad phayton1.py
@@ -0,0 +1,15 @@
+c=3
+x=4
+y=7
+z=6
+
+2-=1
+y*=4
+x**=2
+
+#comentar 
+print(not(x > z)) or x < y and y < x ) 
+
+print("not(x < y and z > x) or x > y and z < y)*) 
+R = not(x > z or not (x < y and z > x ) or x > y and z < y)
+print("El resultado es :) (R))
